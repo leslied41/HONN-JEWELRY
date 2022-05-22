45beedd19102eaf6e20aef1795c7e9bf4e0d2b98
leslied41 <leslied41@gmail.com>
2022-05-22 18:15:40 +1000

    upload

--- site/pages/instagram.tsx
+++ site/pages/instagram.tsx
@@ -1,41 +1,89 @@
 import React, { useState, useEffect } from 'react'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import { GetServerSideProps } from 'next'
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  let token = process.env.IG_LONG_LIVED_ACCESS_TOKEN
-  console.log(token)
-  //   const response = await fetch(
-  //     `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${token}`
-  //   )
-  //   const long_live_obj = await response.json()
-  //   const { access_token,expires_in } = long_live_obj
-  const data = await fetch(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=${token}`
-  )
-  const processed_data = await data.json()
-  const final_data = processed_data.data
-  //if current time > startting time+gap, refresh token and set starting time to current time.
-  //如果知道token的expire时间，那么60-expiry就是创建时间，那么现在的时间-创建时间=30天，就refresh。
-
-  return {
-    props: { final_data }, // will be passed to the page component as props
-  }
-}
+const axios = require('axios').default
 
 interface Props {
-  final_data: {
-    id?: string
-    caption?: string
+  data: {
     media_url?: string
+    id: string
+    caption?: string
   }[]
 }
 
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  ) //this is used to cache dynamic response.
+
+  const getStartAndToken = async () => {
+    const res = await fetch(`${process.env.FETCH_START_AND_TOKEN_URL}/ig/get`)
+    const data = await res.json()
+    return {
+      start: data.ig_obj[0].start_time,
+      access_key: data.ig_obj[0].access_key,
+    }
+  }
+  const update = async (time: Number) => {
+    const id = '1653136746962' //this id is used to identify the {start_time,access_token} in mangodb.
+    //code refresh token here
+    const access_key = await refreshToken()
+    //update the access_keu and start_time in mangodb.
+    axios
+      .patch(`${process.env.FETCH_START_AND_TOKEN_URL}/ig/update/${id}`, {
+        start_time: time,
+        access_key: access_key,
+      })
+      .then((res: any) => {
+        //console.log(res)
+      })
+      .catch((err: any) => console.log(err))
+  }
+  const refreshToken = async () => {
+    const response: any = await fetch(
+      `${process.env.IG_REFRESH_TOKEN_URL}${access_key}`
+    )
+    const long_live_obj = await response.json()
+    const { access_token, expires_in } = long_live_obj
+    return access_token
+  }
+  const getData = async () => {
+    const raw_data = await fetch(
+      `${process.env.IG_FETCH_DATA_URL}${access_key}`
+    )
+    const processed_data = await raw_data.json()
+    const data = processed_data.data
+    return data
+  }
+  //execution lines starting from below
+  let { start, access_key } = await getStartAndToken()
+  const data = await getData()
+
+  if (Date.now() - start > 2592000000) {
+    start = Date.now()
+    update(start)
+  }
+
+  if (!data) {
+    start = Date.now()
+    update(start)
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
+
 const Instagram = (props: Props) => {
-  const data = props.final_data
+  const data = props.data
   return (
     <div>
-      {data?.map((i) => {
-        const { id, media_url, caption } = i
+      {data?.map((item) => {
+        const { id, media_url, caption } = item
         return <img src={media_url} alt={caption} key={id} />
       })}
     </div>
