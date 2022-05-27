a3c53f376ccce5b05b53a42f9f5f020baaf79093
leslie <leslied41@gmail.com>
2022-05-27 13:29:07 +1000

    fix bug

--- site/components/ui/ProductLayoutContext.tsx
+++ site/components/ui/ProductLayoutContext.tsx
@@ -68,7 +68,10 @@ export const ProductLayoutProvider: React.FC<React.ReactNode> = ({
   const value = useMemo(
     () => ({ ...state, setColor, setShape, setProducts }),
     [state]
-  )
+  ) //useMemo can memorize this value and this value will not be regenerated when this componet gets rerendered.
+  //Due to referencial equality, everytime this compoent rerender, this value would be created again and it means that
+  //this value inside ProductLayoutContext.Provider would be updated everytime compoent rerender, making all the component
+  //consuming this useContext will get rerendered.
   return (
     <ProductLayoutContext.Provider value={value}>
       {children}
